307aef20445b569fbc09d6a41237cc4a5c143859
Laszlo Gal <laszlo@geogebra.org>
2021-01-12 10:22:02 +0100

    APPS-2471 sqrt fix

--- common/src/main/java/org/geogebra/common/kernel/interval/IntervalAlgebra.java
+++ common/src/main/java/org/geogebra/common/kernel/interval/IntervalAlgebra.java
@@ -134,7 +134,7 @@ class IntervalAlgebra {
 			interval.setEmpty();
 			return interval;
 		}
-		if (interval.isWhole() || interval.isZero()) {
+		if (interval.isWhole()) {
 			return IntervalConstants.whole();
 		}
 		return nthRoot(2);
