81ca258405599275aaf80df7782392e09652d224
Laszlo Gal <laszlo@geogebra.org>
2021-01-14 09:56:06 +0100

    APPS-2471 code cleaning

--- common/src/main/java/org/geogebra/common/kernel/interval/IntervalAsymptotes.java
+++ common/src/main/java/org/geogebra/common/kernel/interval/IntervalAsymptotes.java
@@ -1,7 +1,5 @@
 package org.geogebra.common.kernel.interval;
 
-import org.geogebra.common.util.debug.Log;
-
 public class IntervalAsymptotes {
 	private final IntervalTupleList samples;
 	private final IntervalTuple range;
@@ -39,73 +37,12 @@ public class IntervalAsymptotes {
 		}
 	}
 
-
-	private void fixAt(int index) {
-		Interval right = rightValue(index);
-		Interval left = leftValue(index);
-		if (!left.isWhole() && isCutOffPoint(index) && !right.isWhole()) {
-			if (left.isEmpty() && !right.isEmpty())  {
-				connectFromRight(index, right);
-				value(index).setEmpty();
-			} else if (!left.isEmpty() && right.isEmpty())  {
-				connectFromLeft(index, left);
-				value(index).setEmpty();
-			} else {
-				if (isVerticalAsymptote(right, left)) {
-					connect(left, right);
-					value(index).setEmpty();
-				} else {
-					value(index).set(left);
-				}
-			}
-		}
-	}
-
 	private boolean isVerticalAsymptote(Interval right, Interval left) {
 		if (left.isEmpty() || right.isEmpty()) {
 			return true;
 		}
 
 		return Math.abs(left.getLow() - right.getLow()) >= range.y().getLow();
-	}
-
-	private void connectFromLeft(int index, Interval left) {
-		if (value(index -2 ).getLow() < left.getLow()) {
-			left.setHigh(range.y().getHigh());
-		}  else {
-			left.setLow(range.y().getLow());
-		}
-	}
-
-	private void connectFromRight(int index, Interval right) {
-		if (right.getLow() > value(index + 2).getLow()) {
-			right.setHigh(range.y().getHigh());
-		} else {
-			right.setLow(range.y().getLow());
-		}
-	}
-
-	private boolean isCutOffPoint(int index) {
-		return samples.get(index).y().isWhole();
-	} 
-
-	private void connect(Interval left, Interval right) {
-		if (!left.isOverlap(right)) {
-			if (left.getLow() > right.getHigh()) {
-				left.setHigh(Math.max(left.getHigh(), range.y().getHigh()));
-				right.setLow(Math.min(range.y().getLow(), right.getLow()));
-			}
-
-			if (left.getHigh() < right.getLow()) {
-				left.setLow(Math.min(left.getLow(), range.y().getLow()));
-				right.setHigh(Math.max(range.y().getHigh(), right.getHigh()));
-			}
-		}
-	}
-
-
-	private void log(String message) {
-		Log.debug("[ASYM] " + message);
 	}
 
 	private Interval value(int index) {
@@ -130,5 +67,4 @@ public class IntervalAsymptotes {
 	private IntervalTuple next(int index) {
 		return samples.get(index + 1);
 	}
-
 }
